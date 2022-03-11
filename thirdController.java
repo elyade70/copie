eb08bb90da8fae80b4bff761febd4664036e20d6
elyade70 <92145687+elyade70@users.noreply.github.com>
2022-03-11 11:19:45 +0100

    Add files via upload

---
+++ thirdController.java
@@ -0,0 +1,31 @@
+package fr.yade;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class thirdController {
+
+    @FXML
+    private TextField txtnuitééqté;
+
+    @FXML
+    private TextField txtnuitéemu;
+
+    @FXML
+    private TextField txtnuitéett;
+
+    @FXML
+    private TextField txtrepasqté;
+
+    @FXML
+    private TextField txtrepasmu;
+
+    @FXML
+    private TextField txtrepastt;
+
+    public void initialize() {
+
+        txtnuitéemu.setText("ekip");
+
+    }
+}
